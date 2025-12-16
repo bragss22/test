def45e6a994312fe62fe15964cec56d1ff4583eb
Ivan Karpenko <karpenko_8585@mail.ru>
2025-12-16 16:43:07 +0300

    -add Exception user already exists

--- app/repos/user_repo.py
+++ app/repos/user_repo.py
@@ -1,11 +1,10 @@
-from sqlalchemy import insert
-from sqlalchemy.ext.asyncio import AsyncSession
+import logging
 
 from core.security import hash_password
 from models.user import User
 from schemas.user import UserCreate
-
-import logging
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 logger = logging.getLogger(__name__)
 
@@ -18,14 +17,14 @@ class UserRepository:
     async def register(self, user_create: UserCreate) -> User:
         # Хешируем пароль
         logger.info(f"[HASH] Получен пароль: '{user_create.password}'")
-        logger.info(f"[HASH] Длина в символах: {len(user_create.password)}")
+        logger.info(f'[HASH] Длина в символах: {len(user_create.password)}')
         logger.info(f"[HASH] Длина в байтах: {len(user_create.password.encode('utf-8'))}")
         hashed_password = hash_password(user_create.password)
 
         # Создаем объект User для вставки
         user_data = {
-            "email": user_create.email,
-            "hashed_password": hashed_password
+            'email': user_create.email,
+            'hashed_password': hashed_password
         }
 
         # Выполняем вставку
@@ -40,3 +39,8 @@ class UserRepository:
         await self.db.refresh(user)
 
         return user
+
+    async def get_user(self, email: str) -> User:
+        stmt = select(User).where(User.email == email)
+        res = await self.db.execute(stmt)
+        return res.scalars().first()
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,8 @@
-from repos.user_repo import UserRepository
+from core.security import create_access_token, verify_password
+from fastapi import HTTPException, status
 from models.user import User
-from schemas.user import UserCreate
+from repos.user_repo import UserRepository
+from schemas.user import Token, UserCreate
 
 
 class UserService:
@@ -10,5 +12,16 @@ class UserService:
 
     async def register(self, user: UserCreate) -> User:
         """Создать пользователя."""
-        return await self.repo.register(user_create=user)
+        try:
+            user = await self.repo.register(user_create=user)
+        except Exception:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Такой пользователь уже есть в БД')
+        return user
 
+    async def get_token(self, email: str, password: str) -> Token:
+        """Получение токена."""
+        user = await self.repo.get_user(email=email)
+        if not user or not verify_password(password, user.hashed_password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
+        access = create_access_token(subject=str(user.id))
+        return Token(access_token=access)
